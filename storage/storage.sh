e5a1670fd334733c74b8e030010f0310c04174af
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2023-11-10 09:11:12 -0300

    Update storage.sh

--- storage/storage.sh
+++ storage/storage.sh
@@ -21,8 +21,8 @@ tune2fs -L samba /dev/md0
 mkdir -pv /srv/cdpni/samba/
 echo -e "LABEL=samba \t\t /srv/cdpni/samba/ \t\t ext4  defaults 0 0"  >> /etc/fstab
 mount -a
-for i in homes drivers lixeiras administrativo aevp almoxarifado canil cimic cpd dcsd educacao financas inclusao infraestrutura publico saude scanner sindicancia supervisao wallpaper
-chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II
-portaria_turno_III portaria_turno_IV rol_de_visitas; do mkdir -pv /srv/cdpni/samba/$i; done
+for i in homes drivers lixeiras administrativo aevp almoxarifado canil cimic cpd dcsd educacao financas inclusao infraestrutura publico saude scanner sindicancia supervisao wallpaper; do mkdir -pv /srv/cdpni/samba/$i; done
+for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar diretoria_geral diretoria_de_centro nucleo_de_pessoal; do mkdir -pv /srv/cdpni/samba/$i; done
+for i in portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas; do mkdir -pv /srv/cdpni/samba/$i; done
 chmod a+w /srv/cdpni/samba/*
 init 6
