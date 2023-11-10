be25176e4e64bd90037e8a41b2e20965c37250c1
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2023-11-10 09:34:46 -0300

    Update storage.sh

--- storage/storage.sh
+++ storage/storage.sh
@@ -13,7 +13,7 @@ cp -v /root/cdpni/storage/etc/motd  /etc/
 cp -v /root/cdpni/storage/etc/resolv.conf /etc/
 	chattr +i /etc/resolv.conf
 apt install openssh-server mdadm -y
-#cp -v /root/cdpni/storage/ssh/sshd_config /etc/ssh/
+cp -v /root/cdpni/storage/ssh/sshd_config /etc/ssh/
 	#systemctl restart sshd
 mdadm --create /dev/md0 --level=5 --raid-devices=4 /dev/sdb /dev/sdc /dev/sdd /dev/sde
 mkfs -t ext4 /dev/md0
