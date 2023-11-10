3ec2ddd5795286bb3237426b4c6e006713e42ac1
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2023-11-10 16:04:35 -0300

    Update samba.sh

--- samba/samba.sh
+++ samba/samba.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apt update 
 hostnamectl set-hostname ad.cdpni.sap
 apt install openssh-server nfs-client samba krb5-config winbind krb5-user smbclient -y
 cp -v /root/cdpni/samba/etc/network/interfaces /etc/network
