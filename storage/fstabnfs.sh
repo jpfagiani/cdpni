bc50df0815d894731d713e63195708a1de2c975a
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2023-11-12 11:10:05 -0300

    Update fstabnfs.sh

--- storage/fstabnfs.sh
+++ storage/fstabnfs.sh
@@ -4,5 +4,5 @@ for i in homes drivers lixeiras administrativo aevp almoxarifado canil cimic cpd
 chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar diretoria_geral diretoria_de_centro nucleo_de_pessoal
 portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas; 
 echo "10.14.29.11:/srv/samba/$i  /srv/samba/$i nfs defaults 0 0 " >> /etc/fstab ; 
-chmod a+w /srv/cdpni/samba/$i
+chmod a+w /srv/samba/$i
 done
